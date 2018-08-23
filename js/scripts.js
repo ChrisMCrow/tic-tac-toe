b94b6e377d488798a7477a7743aa62c1d31544dc
Akjol Jaenbai and Chris Crow <chris@>
2018-08-23 13:51:39 -0700

    refactor code to make more dry.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,17 @@
-var playerX = {mark: "X", turn: false, spaces: []};
-var playerO = {mark: "O", turn: true, spaces: []};
+var playerX = {mark: "X", turn: true, spaces: []};
+var playerO = {mark: "O", spaces: []};
+var tie = true
 
-var condition1 = ["b1", "b2", "b3"]
 var winConditions = [
-  ["b1", "b2", "b3"]
-  ["b1", "b4", "b7"]
+  ["b1", "b2", "b3"],
+  ["b1", "b4", "b7"],
+  ["b1", "b5", "b9"],
+  ["b2", "b5", "b8"],
+  ["b3", "b5", "b7"],
+  ["b3", "b6", "b9"],
+  ["b4", "b5", "b6"],
+  ["b7", "b8", "b9"]
 ]
-// condition2 =
-// condition3 = ["b1", "b2", "b3"]
-// condition1 = ["b1", "b2", "b3"]
-// condition1 = ["b1", "b2", "b3"]
-// condition1 = ["b1", "b2", "b3"]
-// condition1 = ["b1", "b2", "b3"]
-// condition1 = ["b1", "b2", "b3"]
-
 
 function mark() {
   if (playerX.turn === true) {
@@ -27,16 +25,21 @@ function mark() {
   }
 }
 
-function checkForWin(conditions, playerSpaces) {
-  console.log(conditions);
-  console.log(playerSpaces);
-  // conditions.forEach(function(condition) {
-    for(var i = 0; i < conditions.length; i++) {
-      if($.inArray(conditions[i], playerSpaces) == -1)
-      return false;
+function checkForWin(conditions, playerSpaces, playerMark) {
+  conditions.forEach(function(condition) {
+    for(var i = 0; i < condition.length; i++) {
+      if (playerSpaces.includes(condition[i]) == false) return false;
     }
+    tie = false
+    alert(playerMark + " wins!")
     return true;
-  // });
+  });
+}
+
+function checkForTie() {
+  if (playerX.spaces.length + playerO.spaces.length == 9 && tie == true) {
+    alert("It's a tie!")
+  }
 }
 
 $(document).ready(function() {
@@ -44,10 +47,11 @@ $(document).ready(function() {
     boxID = $(this).attr("id");
     $(this).text(mark());
     $(this).off("click");
-    // checkForWin(condition1, playerX.spaces);
-    if (checkForWin(condition1, playerO.spaces)) {alert("O wins!")}
-    if (checkForWin(condition1, playerX.spaces)) {alert("X wins!")}
+    checkForWin(winConditions, playerO.spaces, playerO.mark);
+    checkForWin(winConditions, playerX.spaces, playerX.mark);
+    checkForTie();
   });
+
   $("button").click(function() {
     document.location.reload();
   });
